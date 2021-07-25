bc329501ce1ec07b976e5045a634d64654c1078d
avbokov <bokov.itstep@gmail.com>
2021-07-25 23:25:18 +0700

    Init

---
+++ Arrays/Source.cpp
@@ -0,0 +1,220 @@
+﻿#include<iostream>
+
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+void FillRand(int arr[], const int n);
+void FillRand(int arr2D[][], const int ROWS, const int COLS);
+
+void Print(int arr[], const int n);
+void Sort(int arr[], const int n);
+int Sum(int arr[], const int n);
+double AVG(int arr[], const int n);
+int minValueIn(int arr[], const int n);
+int maxValueIn(int arr[], const int n);
+void shiftLeft(int arr[], const int n, int number_of_shifts);
+void shiftRight(int arr[], const int n, int number_of_shifts);
+void shiftRight_V2(int arr[], const int n, int number_of_shifts);
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int n = 5;
+	int arr[n];
+	int number_of_shifts;
+	
+	const int ROWS = 8;
+	const int COLS = 5;
+	int arr2D[ROWS][COLS];	
+		
+	FillRand(arr, n);
+	Print(arr, n);
+	cout << endl << endl;
+	cout << "Введите количество шагов для сдвига влево: "; cin >> number_of_shifts;
+	cout << endl << endl;
+	shiftLeft(arr, n, number_of_shifts);
+	cout << "Массив сдвинут влево на " << number_of_shifts << " шаг(а)-(ов):";
+	cout << endl << endl;
+	Print(arr, n);
+
+	cout << endl << endl;
+	cout << "Введите количество шагов для сдвига вправо: "; cin >> number_of_shifts;
+	cout << endl << endl;
+	shiftRight(arr, n, number_of_shifts);
+	cout << "Массив сдвинут вправо на " << number_of_shifts << " шаг(а)-(ов):";
+	cout << endl << endl;
+	Print(arr, n);
+
+	cout << endl << endl;
+	cout << "Введите количество шагов для сдвига вправо: "; cin >> number_of_shifts;
+	cout << endl << endl;
+	shiftRight_V2(arr, n, number_of_shifts);
+	cout << "Массив сдвинут вправо на " << number_of_shifts << " шаг(а)-(ов):";
+	cout << endl << endl;
+	Print(arr, n);
+
+	cout << endl << endl;
+	cout << "Отсортируем массив по возрастанию: ";
+	cout << endl << endl;
+	Sort(arr, n);
+	cout << endl;
+	Print(arr, n);
+
+	cout << endl << endl;
+	cout << "Сумма элементов массива равна: " << Sum(arr, n) << endl;
+	cout << endl << endl;
+	cout << "Среднее арифметическое элементов массива равно: " << AVG(arr, n) << endl;
+	cout << endl << endl;
+	cout << "Минимальное значение: " << minValueIn(arr, n) << endl;
+	cout << endl << endl;
+	cout << "Максимальное значение: " << maxValueIn(arr, n) << endl;
+		
+	cout << endl << endl;
+
+	const int B_SIZE = 8;
+	int brr[B_SIZE];
+	FillRand(brr, B_SIZE);
+	Print(brr, B_SIZE);
+	Sort(brr, B_SIZE);
+	cout << endl;
+	Print(brr, B_SIZE);
+
+	cout << endl << endl;
+	cout << "Сумма элементов массива равна: " << Sum(brr, B_SIZE) << endl;
+	cout << endl << endl;
+	cout << "Среднее арифметическое элементов массива равно: " << AVG(brr, B_SIZE) << endl;
+	cout << endl << endl;
+	cout << "Минимальное значение: " << minValueIn(brr, B_SIZE) << endl;
+	cout << endl << endl;
+	cout << "Максимальное значение: " << maxValueIn(brr, B_SIZE) << endl;
+}
+
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+
+void FillRand(int arr2D[][], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr2D[i][j] = rand() % 100;
+		}
+		cout << endl;
+	}
+}
+
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+}
+
+void Sort(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+
+int Sum(int arr[], const int n)
+{
+	int S = 0;
+	
+	for (int i = 0; i < n; i++)
+	{
+		S = S + arr[i];
+	}
+	return S;
+}
+
+double AVG(int arr[], const int n)
+{
+	double avg;
+
+	avg = (double)Sum(arr, n) / n;
+
+	return avg;
+}
+
+int minValueIn(int arr[], const int n)
+{
+	int min = arr[0];
+
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)
+		{
+			min = arr[i];
+		}
+	}
+	return min;
+}
+
+int maxValueIn(int arr[], const int n)
+{
+	int max = arr[0];
+
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+	return max;
+}
+
+void shiftLeft(int arr[], const int n, int number_of_shifts)
+{
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[0];
+
+		for (int i = 1; i < n; i++)
+		{
+			arr[i - 1] = arr[i];
+		}
+		arr[n - 1] = buffer;
+	}
+}
+
+void shiftRight(int arr[], const int n, int number_of_shifts)
+{
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[n - 1];
+
+		for (int i = n - 1; i > 0; i--)
+		{
+			arr[i] = arr[i - 1];
+		}
+		arr[0] = buffer;
+	}
+}
+
+void shiftRight_V2(int arr[], const int n, int number_of_shifts)
+{
+	number_of_shifts = n - number_of_shifts;
+	shiftLeft(arr, n, number_of_shifts);
+}
---
+++ CW_Functions/Source.cpp
@@ -0,0 +1,97 @@
+﻿// Declaration of variable: объявления переменной
+					//type name;
+
+
+// Declaration of array
+					//type name [SIZE];
+
+
+// Declaration and definition of function:
+					//type (name parameters)  Function header - заголовок функции
+//{
+// тело функции Function body
+// ....
+//	group-of-statements
+// ....
+//}
+// return; // так пишут return в void функциях.
+
+#include<iostream>
+
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+int Add(int a, int b); // Прототип функции (объявление функции - )
+int Sub(int a, int b);
+int Mul(int a, int b);
+double Div(int a, int b);
+
+// Параметры по умолчанию
+double Power(double a, int n = 2);
+// если в функцию передать одно число, то она возведёт в квадрат
+
+int factorial(int n);
+
+void main()
+{ 
+	setlocale(LC_ALL, "Russian");
+
+	cout << Power(5, 3) << endl;
+	cout << factorial(5) << endl;
+
+	int a, b;
+	cout << "Введите два числа: "; cin >> a >> b;
+	//int c = a + b;
+	int c = Add(a, b); // Использование функции, (вызов функции, function call)
+	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << Sub(a, b) << endl;
+	cout << a << " * " << b << " = " << Mul(a, b) << endl;
+	cout << a << " / " << b << " = " << Div(a, b) << endl;
+	cout << a << " ^ " << b << " = " << Power(a, b) << endl;
+	
+
+}
+// additional - сложение
+int Add(int a, int b) 
+{
+	// В теле функции мы определяем, что делает эта функция
+	// Реализуем алгоритм, возложенный на функцию
+	int c = a + b;
+	return c;
+	// В теле функции можно писать всё, что угодно, кроме РЕАЛИЗАЦИИ другой функции
+}
+
+int Sub(int a, int b) // subtraction - вычитание
+{
+	return a - b;
+}
+
+int Mul(int a, int b) // multiplication - умножение
+{
+	return a * b;
+}
+
+double Div(int a, int b) // division - деление
+{
+	return (double)a / b;
+}
+
+double Power(double a, int n) // division - деление
+{
+	double N = 1;
+	if (n < 0)
+	{
+		a = 1 / a;
+		n = -n;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		N *= a;
+	}
+	return N;
+}
+
